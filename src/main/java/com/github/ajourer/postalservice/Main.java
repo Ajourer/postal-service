485cfa94875cdec4b3c955903628f0634ada11eb
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-09-28 20:22:40 +0100

    next

--- src/main/java/com/github/ajourer/postalservice/Main.java
+++ src/main/java/com/github/ajourer/postalservice/Main.java
@@ -1,6 +1,7 @@
 package com.github.ajourer.postalservice;
 
 import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import static org.slf4j.LoggerFactory.getLogger;
