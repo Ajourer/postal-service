53e61c4dfbf407a7104ec56047e247d2bbee7226
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-10-04 19:10:56 +0100

    next

--- src/main/java/com/github/ajourer/postalservice/Main.java
+++ src/main/java/com/github/ajourer/postalservice/Main.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 public class Main {
     private static final Logger log = getLogger(Main.class);
 
-    public static void main(final String[] args) throws SQLException {
+    public static void main(final String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
