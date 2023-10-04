4b34c5c433f8c222a784780b1eef9ce33f75473e
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-10-04 19:05:46 +0100

    next

--- src/main/java/com/github/ajourer/postalservice/Main.java
+++ src/main/java/com/github/ajourer/postalservice/Main.java
@@ -15,20 +15,6 @@ public class Main {
     private static final Logger log = getLogger(Main.class);
 
     public static void main(final String[] args) throws SQLException {
-        try(
-            final var connection = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/postal_service",
-                "postal_service",
-                "a2{O)9U%V+Lt~dnMykSPG1?sF8Wf:p0H"
-            );
-            final var statement = connection.createStatement()
-        ) {
-            final var result = statement.executeQuery(
-                "SELECT * FROM pg_catalog.pg_user"
-            );
-            System.out.println(result.next());
-            System.out.println(result.getString(1));
-        }
         SpringApplication.run(Main.class, args);
     }
 
