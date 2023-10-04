89fc2fefb1987bbb4b55ee717ea694a28b59e3fb
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-10-04 19:55:43 +0100

    next

--- src/main/java/com/github/ajourer/postalservice/Main.java
+++ src/main/java/com/github/ajourer/postalservice/Main.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @SpringBootApplication
