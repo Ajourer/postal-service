43c5809b18dc47c58ad34c99dfe662076b44c3a6
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-10-04 19:54:43 +0100

    next

--- src/main/java/com/github/ajourer/postalservice/Main.java
+++ src/main/java/com/github/ajourer/postalservice/Main.java
@@ -14,6 +14,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@PropertySource("classpath:/application.properties")
 public class Main {
     private static final Logger log = getLogger(Main.class);
 
