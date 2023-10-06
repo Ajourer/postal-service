84dd2e88cb43ffb4d1eeea125cade7c949623336
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-10-06 19:47:06 +0100

    next

--- database/script.sql
+++ database/script.sql
@@ -4,7 +4,7 @@ DROP USER IF EXISTS postal_service;
 
 -- Just for example. You should not save password to git.
 CREATE USER postal_service WITH
-    UNENCRYPTED PASSWORD 'a2{O)9U%V+Lt~dnMykSPG1?sF8Wf:p0H';
+    ENCRYPTED PASSWORD 'a2{O)9U%V+Lt~dnMykSPG1?sF8Wf:p0H';
 CREATE DATABASE postal_service WITH
     OWNER postal_service
     ENCODING 'UTF8';
