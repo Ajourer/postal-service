e62bf476bb981a8a542107934b50c40a06f866c9
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-11-08 21:14:09 +0000

    go ahead

--- src/main/java/com/github/ajourer/postalservice/PostalItemController.java
+++ src/main/java/com/github/ajourer/postalservice/PostalItemController.java
@@ -1,6 +1,7 @@
 package com.github.ajourer.postalservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/postal-items")
 @RestController
+@SpringBootApplication
 public class PostalItemController {
     private final PostalItemRepository repository;
 
