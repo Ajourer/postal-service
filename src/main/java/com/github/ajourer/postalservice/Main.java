fa4fc237a9381de0797d9fc01499c77a06ae15a6
Ajourer <levandovski.3@gmail.com>
2024-01-10 22:39:54 +0300

    next

--- src/main/java/com/github/ajourer/postalservice/Main.java
+++ src/main/java/com/github/ajourer/postalservice/Main.java
@@ -1,10 +1,8 @@
 package com.github.ajourer.postalservice;
 
 import org.slf4j.Logger;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import static org.slf4j.LoggerFactory.getLogger;
 
