4e3f00cd7570ea1170f7ff6414436f6bdf4ee954
Ajourer <levandovski.3@gmail.com>
2024-01-10 23:33:45 +0300

    next

--- src/main/java/com/github/ajourer/postalservice/PostalItemController.java
+++ src/main/java/com/github/ajourer/postalservice/PostalItemController.java
@@ -1,7 +1,6 @@
 package com.github.ajourer.postalservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
